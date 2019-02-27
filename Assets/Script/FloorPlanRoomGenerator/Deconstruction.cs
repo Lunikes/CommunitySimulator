596bcfa23cba181281ab1aae94e1ae60c1e98afe
Lunikes <1368393448@qq.com>
2019-02-27 09:47:42 +0800

    Deconstruction and rotation
    -Now can enter Deconstruction mode to deconstruct cube and prefab, even panel
    -Now can rotate prefab that you placed before

---
+++ Assets/Script/FloorPlanRoomGenerator/Deconstruction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deconstruction : MonoBehaviour
+{
+
+    bool isactive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isactive = false;
+        }
+        if (isactive)
+        {
+            deletionMode();
+        }
+        else {
+            
+        }
+     
+    }
+    public void deletionMode()
+    {
+       
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    BoxCollider cube = hit.collider as BoxCollider;
+                    if (cube != null)
+                    {
+
+                        Destroy(cube.gameObject);
+
+                    }
+                }
+            }
+        }
+    public void Trigger()
+    {
+        isactive = !isactive;
+    }
+    public void fastExit() {
+      
+    }
+
+}
+
+
