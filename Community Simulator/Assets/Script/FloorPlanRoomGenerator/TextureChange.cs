d27d871bd5558d9fcd6137e333a14b0c2380b9cf
jacobhac96 <Riley#1996>
2019-04-02 22:53:41 -0400

    Added more assets to community map
    -grass was added
    -trees were added
    -a second building was added
    -All of this is just 3d models no scripts are added for this update as it was just to add more terrian elements

---
+++ Community Simulator/Assets/Script/FloorPlanRoomGenerator/TextureChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChange : MonoBehaviour
+{
+
+    public Texture[] textures;
+    public int currentTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            currentTexture++;
+            currentTexture %= textures.Length;
+            GetComponent<Renderer>().material.mainTexture = textures[currentTexture];
+        }
+    }
+}
