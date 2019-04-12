74c53bfdb93b57a5a7b81d6b577fdef7931d09af
jacobhac96 <Riley#1996>
2019-04-12 07:58:23 -0400

    test Cases
    This is the initial test cases push
    -the tests for the branches and the corresponding features
    -each feature in the zenhub has a test case

---
+++ Community Simulator/Assets/Tests/floormanager/floorsize.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class floorsize
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void floorsizeSimplePasses()
+        {
+            // Use the Assert class to test conditions
+
+            //Vector3 realposition = 
+
+            Debug.Log("hihi");
+
+
+
+
+
+
+
+
+        }
+    }
+}
