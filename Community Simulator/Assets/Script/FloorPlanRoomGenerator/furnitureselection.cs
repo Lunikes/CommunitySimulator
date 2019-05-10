f45f0922416371a06d44230a8db7a8f7cbb54ac4
jacobhac96 <Riley#1996>
2019-05-10 03:55:53 -0400

    furniture model selection
    you can now select furniture models based on dropdown selection menu in the 3d edit mode

---
+++ Community Simulator/Assets/Script/FloorPlanRoomGenerator/furnitureselection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class furnitureselection : MonoBehaviour
+{
+    private List<Transform> models;
+
+    public GameObject hideObject;
+    public GameObject hideObjectCamera;
+
+    bool onOff = false;
+
+    public void furnitureModelMode()
+    {
+        onOff = !onOff;
+        hideObject.SetActive(!onOff);
+        hideObjectCamera.SetActive(!onOff);
+        unHideObject.SetActive(onOff);
+        arrowR.SetActive(onOff);
+        arrowL.SetActive(onOff);
+    }
+
+
+
+    int select = 0;
+
+    public void Selector(int num)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var model = transform.GetChild(i);
+            models.Add(model);
+            model.gameObject.SetActive(i == select);
+        }
+    }
+
+
+
+
+    public void Awake()
+    {
+        models = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var model = transform.GetChild(i);
+            models.Add(model);
+
+            model.gameObject.SetActive(i == 0);
+        }
+    }
+}
+
+}
